61b42a3363890c7412ea8a2fae76673ebfbb4b20
M Fadhly NR <33947296+maslow123@users.noreply.github.com>
2019-05-25 19:33:12 +0700

    Update 3.php

--- 3.php
+++ 3.php
@@ -3,12 +3,12 @@
     /* Author : M.Fadhly Noor Rizqi */
     function hitungHurufVokal($parameter)
     {
-        $data = substr_count($parameter, 'a')+substr_count($parameter, 'i')+substr_count($parameter, 'u')+substr_count($parameter, 'e')+substr_count($parameter, 'o');
+        $getData = substr_count($parameter, 'a')+substr_count($parameter, 'i')+substr_count($parameter, 'u')+substr_count($parameter, 'e')+substr_count($parameter, 'o');
 
-        return $data;
+        return $getData;
     }
 
-    $parameter = "programmer";
-    $result = hitungHurufVokal($parameter);
+    $result = hitungHurufVokal('hm..');
+    echo "result : ".$result;
 
-    echo "Jumlah huruf vokal didalam kata <b>".$parameter. "</b> yaitu berjumlah <b>".$result."</b> huruf";
+    

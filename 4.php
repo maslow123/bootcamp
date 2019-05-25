a42b71decd732fa6150eb4ba5124006995aa0af3
M Fadhly NR <33947296+maslow123@users.noreply.github.com>
2019-05-25 19:34:25 +0700

    Update 4.php

--- 4.php
+++ 4.php
@@ -1,25 +1,25 @@
 <?php
     /* Author : M.Fadhly Noor Rizqi */
-    
+
     function kotak($param)
     {
-        $sisi = $param;
+        $angka = $param;
         $j;
         $i;
 
-        for($i = 0; $i<$sisi;$i++)
+        for($i = 0; $i<$angka;$i++)
         {
             echo "#";
-            for($j=0;$j<$sisi;$j++)
+            for($j=0;$j<$angka;$j++)
             {
 
-                if($i==0 || $sisi-3)
+                if($i==0 || $angka-3)
                 {
                     echo "=";
                 }
                 else
                 {
-                    if($j==0 || $j== $sisi-2)
+                    if($j==0 || $j== $angka-2)
                     {
                         echo "#";
                     }
